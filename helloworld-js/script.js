3b91def6d5ac527f1e64e409452e8d422ca8f3ca
Charlotte <45144343+charcambau@users.noreply.github.com>
2019-04-04 12:57:57 +0200

    intro to JS

---
+++ helloworld-js/script.js
@@ -0,0 +1,32 @@
+//single line comment
+
+/*
+multi
+line
+comment 
+*/
+
+//To log information to the console
+//We can use the console.log function
+
+console.log('hello world');
+//this line is a statement
+//single line instruction
+//has to end with;
+
+console.log('goodbye');
+
+
+//tested the var alone
+var x = 42
+
+console.log(x + x);
+
+console.log('I am ' + x + ' years old');
+
+
+//tested the var with teacher
+var messageToTheWorld = 'Hello World';
+var myName = 'Charlotte';
+
+console.log(messageToTheWorld + ' from ' + myName);
