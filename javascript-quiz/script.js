0944bd3e4d54142754a93ab52962bb2c72f02b48
charcambau <45144343+charcambau@users.noreply.github.com>
2019-05-16 00:07:49 +0200

    javascript ex

---
+++ javascript-quiz/script.js
@@ -0,0 +1,10 @@
+
+
+var selectAnswerGoldfish = document.getElementById('yesGoldfish');
+selectAnswerGoldfish.addEventListener('click', selectwrongAnswer);
+
+function selectwrongAnswer () {
+  console.log('does this work');
+
+  document.getElementsById('yesGoldfish')
+}
