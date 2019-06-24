4fd5dce0e3aa4e415cc959269f6d0489cae33640
Charlotte <45144343+charcambau@users.noreply.github.com>
2019-06-24 13:05:22 +0200

    Revert "js"
    This reverts commit 2aacc82fc0e7b9b63a8dd1d025acf83bf30d3bb2, reversing
    changes made to fe0082fe08e803c4421390ded32f3bfa7dea8c16.

--- dolphins-layout/script.js
+++ dolphins-layout/script.js
@@ -1,70 +1,12 @@
-<<<<<<< HEAD
 
 var dotClicked;
 
 var toNextSlideTwo = document.getElementById('nextSlideTwo');
 toNextSlideTwo.addEventListener('click', nextSlideTwo);
-=======
-// VARIABLES
-var btnClicked;
-var nextSlide;
-var lastSlide = "dolphin";
-var btnOne = document.getElementById('btn-dolphin');
-var btnTwo = document.getElementById('btn-palm');
-var btnThree = document.getElementById('btn-earth');
-
-// EVENT LISTENERS
-btnOne.addEventListener('click', changeSlide);
-btnTwo.addEventListener('click', changeSlide);
-btnThree.addEventListener('click', changeSlide);
-
-// SIMPLIFIED
-// assign the same function to all buttons
-
-function changeSlide(e) {
-  nextSlide = e.target.dataset.name;
-  // if the statement is true
-  if ( lastSlide == nextSlide) {
-    // return exits (ends) the function
-    // the code after (show, hide,..) doesn't get executed
-    return;
-  }
-  // SHOW
-  document.getElementById(nextSlide).style.display = 'grid';
-  document.getElementById('btn-'+nextSlide).style.backgroundColor = "#2896E0";
-  // HIDE
-  document.getElementById(lastSlide).style.display = 'none';
-  document.getElementById('btn-'+lastSlide).style.backgroundColor = "#BEFCFE";
-  //SAVE
-  lastSlide = nextSlide;
-  
-  playTimeline(nextSlide);
->>>>>>> 5670e6ddf03dca1f922cd62a3084156a40ecef3d
-
-
-}
-
-function playTimeline(nextSlide) {
-  console.log(nextSlide);
-
-  var timelineSlide = new TimelineLite();
-  timelineSlide.from('#' + nextSlide, .3, {x:600, ease: Power1.easeOut});
-  timelineSlide.from('#' + nextSlide + ' .blob', 2, {y:600, ease: Elastic.easeOut}, "0");
-  timelineSlide.from('#' + nextSlide + ' .img', .5, {x:-800, ease: Power1.easeOut}, "0");
-  timelineSlide.from('#' + nextSlide + ' h2', 1, {scale:1.8, ease: Elastic.easeOut}, "0");
-  timelineSlide.from('#' + nextSlide + ' p', 1, {scale:1.5, ease: Elastic.easeOut}, "0" );
-  timelineSlide.from('#' + nextSlide + ' a', .6, {y:800, ease: Back.easeOut}, "0.5");
-}
-
-// FUNCTIONS
-function nextSlideOne() {
-  document.getElementById('earth').style.display = 'none';
-  document.getElementById('palm').style.display = 'none';
-  document.getElementById('dolphin').style.display = 'grid';
-
-}
 
 function nextSlideTwo() {
+  console.log('yup');
+
   document.getElementById('palm').style.display = 'grid';
   document.getElementById('dolphin').style.display = 'none';
   document.getElementById('earth').style.display = 'none';
@@ -75,22 +17,27 @@ function nextSlideTwo() {
   myAnimTwo = TweenLite.from('.island-img', .5, {x:-800, delay:0, ease: Power1.easeOut});
   myAnimTwo = TweenLite.from('#palmTitle', 1, {scale:1.8, delay:0, ease: Elastic.easeOut});
   myAnimTwo = TweenLite.from('#palmText', 1, {scale:1.5, delay:0, ease: Elastic.easeOut});
-<<<<<<< HEAD
   myAnimTwo = TweenLite.from('#palm-link', 1, {y:500, delay:.6, ease: Back.easeOut});
-=======
-  myAnimTwo = TweenLite.from('#palm-link', .6, {y:800, delay:.6, ease: Back.easeOut});
->>>>>>> 5670e6ddf03dca1f922cd62a3084156a40ecef3d
 }
 
+var toNextSlideThree = document.getElementById('nextSlideThree');
+toNextSlideThree.addEventListener('click', nextSlideThree);
+
 function nextSlideThree() {
+  console.log('yes');
+
   document.getElementById('earth').style.display = 'grid';
   document.getElementById('palm').style.display = 'none';
   document.getElementById('dolphin').style.display = 'none';
 
-  
-  btnClicked = toNextSlideThree;
+  myAnimThree = TweenLite.from('#earth', .3, {x:600, delay:0, ease: Power1.easeOut});
 
-<<<<<<< HEAD
+  myAnimThree = TweenLite.from('.earth-blob', 2, {y:600, delay:0, ease: Elastic.easeOut});
+  myAnimThree = TweenLite.from('.earth-img', .5, {x:-800, delay:0, ease: Power1.easeOut});
+  myAnimThree = TweenLite.from('#earthTitle', 1, {scale:1.8, delay:0, ease: Elastic.easeOut});
+  myAnimThree = TweenLite.from('#earthText', 1, {scale:1.5, delay:0, ease: Elastic.easeOut});
+  myAnimThree = TweenLite.from('#earthLink', 1, {y:500, delay:.6, ease: Back.easeOut});
+
   // var dotThree = document.getElementsByClassName('dot-three');
 
   // dotClicked = nextSlideThree;
@@ -98,17 +45,20 @@ function nextSlideThree() {
   // if (dotClicked) {
   //   dotClicked.style.color = 'red';
 
-=======
-  if(btnClicked) {
-    console.log(btnClicked);
-    btnClicked.style.backgroundColor = "red";
-  }
->>>>>>> 5670e6ddf03dca1f922cd62a3084156a40ecef3d
 }
 
+var toNextSlideOne = document.getElementById('nextSlideOne');
+toNextSlideOne.addEventListener('click', nextSlideOne);
 
+function nextSlideOne() {
+  console.log('si');
 
-<<<<<<< HEAD
+  document.getElementById('earth').style.display = 'none';
+  document.getElementById('palm').style.display = 'none';
+  document.getElementById('dolphin').style.display = 'grid';
+
+  myAnimOne = TweenLite.from('#dolphin', .3, {x:600, delay:0, ease: Power1.easeOut});
+
   myAnimOne = TweenLite.from('.dolphin-blob', 2, {y:600, delay:0, ease: Elastic.easeOut});
   myAnimOne = TweenLite.from('.dolphin-img', .5, {x:-800, delay:0, ease: Power1.easeOut});
   myAnimOne = TweenLite.from('#dolphinTitle', 1, {scale:1.8, delay:0, ease: Elastic.easeOut});
@@ -117,9 +67,3 @@ function nextSlideThree() {
 
 }
 
-=======
-// var called current slide: whenever you click on a slide it says current slide:one
-// Compare the clicked slide with the current slide
-// class active to the button you clicked 
-// -- if current is number one and clicked one is number two then make the second one active
->>>>>>> 5670e6ddf03dca1f922cd62a3084156a40ecef3d
