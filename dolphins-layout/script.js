2e75db7d24237d9ce2ed5e7f0316689db2d1840e
Charlotte <45144343+charcambau@users.noreply.github.com>
2019-06-24 13:17:56 +0200

    fixed the conflict

--- dolphins-layout/script.js
+++ dolphins-layout/script.js
@@ -1,8 +1,3 @@
-var dotClicked;
-
-var toNextSlideTwo = document.getElementById('nextSlideTwo');
-toNextSlideTwo.addEventListener('click', nextSlideTwo);
-=======
 // VARIABLES
 var btnClicked;
 var nextSlide;
@@ -37,11 +32,6 @@ function changeSlide(e) {
   lastSlide = nextSlide;
   
   playTimeline(nextSlide);
->>>>>>> 5670e6ddf03dca1f922cd62a3084156a40ecef3d
-
-function nextSlideTwo() {
-  console.log('yup');
-
 }
 
 function playTimeline(nextSlide) {
@@ -55,52 +45,3 @@ function playTimeline(nextSlide) {
   timelineSlide.from('#' + nextSlide + ' p', 1, {scale:1.5, ease: Elastic.easeOut}, "0" );
   timelineSlide.from('#' + nextSlide + ' a', .6, {y:800, ease: Back.easeOut}, "0.5");
 }
-
-// FUNCTIONS
-function nextSlideOne() {
-  document.getElementById('earth').style.display = 'none';
-  document.getElementById('palm').style.display = 'none';
-  document.getElementById('dolphin').style.display = 'grid';
-
-}
-function nextSlideTwo() {
-  document.getElementById('palm').style.display = 'grid';
-  document.getElementById('dolphin').style.display = 'none';
-  document.getElementById('earth').style.display = 'none';
-
-  myAnimTwo = TweenLite.from('#palm', .3, {x:600, delay:0, ease: Power1.easeOut});
-
-  myAnimTwo = TweenLite.from('.island-blob', 2, {y:600, delay:0, ease: Elastic.easeOut});
-  myAnimTwo = TweenLite.from('.island-img', .5, {x:-800, delay:0, ease: Power1.easeOut});
-  myAnimTwo = TweenLite.from('#palmTitle', 1, {scale:1.8, delay:0, ease: Elastic.easeOut});
-  myAnimTwo = TweenLite.from('#palmText', 1, {scale:1.5, delay:0, ease: Elastic.easeOut});
-  myAnimTwo = TweenLite.from('#palm-link', 1, {y:500, delay:.6, ease: Back.easeOut});
-}
-
-function nextSlideThree() {
-  document.getElementById('earth').style.display = 'grid';
-  document.getElementById('palm').style.display = 'none';
-  document.getElementById('dolphin').style.display = 'none';
-
-  
-  btnClicked = toNextSlideThree;
-
-  // var dotThree = document.getElementsByClassName('dot-three');
-
-  // dotClicked = nextSlideThree;
-
-  // if (dotClicked) {
-  //   dotClicked.style.color = 'red';
-
-}
-
-
-
-  myAnimOne = TweenLite.from('.dolphin-blob', 2, {y:600, delay:0, ease: Elastic.easeOut});
-  myAnimOne = TweenLite.from('.dolphin-img', .5, {x:-800, delay:0, ease: Power1.easeOut});
-  myAnimOne = TweenLite.from('#dolphinTitle', 1, {scale:1.8, delay:0, ease: Elastic.easeOut});
-  myAnimOne = TweenLite.from('#dolphinText', 1, {scale:1.5, delay:0, ease: Elastic.easeOut});
-  myAnimOne = TweenLite.from('#dolphin-link', 1, {y:500, delay:.6, ease: Back.easeOut});
-
-}
-
